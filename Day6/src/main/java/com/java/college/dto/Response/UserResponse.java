cfd65ad6cdcc166f799f5707d22673482764e9ea
PAVINO <135719177+PAVINO@users.noreply.github.com>
2024-03-26 10:24:53 +0530

    Add files via upload

---
+++ Day6/src/main/java/com/java/college/dto/Response/UserResponse.java
@@ -0,0 +1,17 @@
+package com.java.college.dto.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private String name;
+    private String email;
+    private String password;
+    private String number;
+}
